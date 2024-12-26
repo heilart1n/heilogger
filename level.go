4385f4c9b4d7dcc3ba854e8bb1c5b2691ab0caa5
heilart1n <heilartin26@gmail.com>
2024-12-27 00:04:38 +0300

    fix

--- level.go
+++ level.go
@@ -3,14 +3,14 @@ package heiloger
 import "log/slog"
 
 // LogLevel defines the level of logging.
-type LogLevel int
+type LogLevel string
 
 // LogLevel constants define the levels of logging, from debug to error.
 const (
-	LevelDebug LogLevel = iota + 4 // LevelDebug represents detailed debugging information.
-	LevelInfo                      // LevelInfo represents general informational messages.
-	LevelWarn                      // LevelWarn indicates potentially harmful situations.
-	LevelError                     // LevelError represents error events that might still allow the application to continue running.
+	LevelDebug LogLevel = "debug" // LevelDebug represents detailed debugging information.
+	LevelInfo  LogLevel = "info"  // LevelInfo represents general informational messages.
+	LevelWarn  LogLevel = "warn"  // LevelWarn indicates potentially harmful situations.
+	LevelError LogLevel = "error" // LevelError represents error events that might still allow the application to continue running.
 )
 
 func slogLevel(level LogLevel) slog.Level {
